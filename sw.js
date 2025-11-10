25f11df21ac69189a5adcbdf49d9fd76b8e14fde
erichernandezbeltran-beep <eric.hernandezbeltran@gmail.com>
2025-11-09 20:20:00 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+
+const CACHE = 'calc-ic-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
